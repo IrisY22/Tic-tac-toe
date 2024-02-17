644fe017fe41f879707056c6bb164e081b30fb1f
Iris Yatskaer <iriska1160@gmail.com>
2024-02-17 14:06:29 +0200

    Add game-board component and funcinality to player component

--- src/cmps/Player.jsx
+++ src/cmps/Player.jsx
@@ -1,11 +1,25 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react"
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [playerName, setPlayerName] = useState(initialName)
+
+  function handleEditClick() {
+    setIsEditing(editing => !editing)
+  }
+
+  function handleChange(e) {
+    setPlayerName(e.target.value)
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing && <span className='player-name'>{playerName}</span>}
+        {isEditing && <input type="text" required value={playerName} onChange={handleChange}></input>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   )
 }
